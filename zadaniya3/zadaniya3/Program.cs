df1f81f9ea357c2bcaded924736789ef82c66ecf
Muhammad <davlatovmuhammad1264@mail.com>
2021-01-28 22:58:16 +0500

    Add zadaniya3/

---
+++ zadaniya3/zadaniya3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace zadaniya3
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int i1 = 0, i2 = 0;
+			for (int a = 0; a < 14; a++)
+			{
+				for (int b = 0; b < 15; b++)
+				{
+
+					if (b == i1 || b == i2) Console.Write("*");
+					else Console.Write(" ");
+				}
+				Console.Write("\n");
+				i2++;
+			}
+			for (int b = 0; b < 15; b++)
+			{
+				Console.Write("*");
+			}
+			Console.Read();
+
+		}
+
+	}
+}
