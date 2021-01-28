a9c18d61c651578e51c8228ccbb0fc367d7e8fce
Muhammad <davlatovmuhammad1264@mail.com>
2021-01-28 22:57:38 +0500

    Add zadaniya2/

---
+++ zadaniya2/zadaniya2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace zadaniya2
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+
+		label: Console.Write("Введить число А = ");
+			double a = double.Parse(Console.ReadLine());
+
+			Console.Write("Введить число B = ");
+			double b = double.Parse(Console.ReadLine());
+
+
+
+			if (a < b)
+			{
+				
+				for (double i = a; a < b; a++)
+				{
+
+					if (a % 2 != 0)
+					{
+						Console.WriteLine("{0,2}", a);
+					}
+				}
+
+
+			}
+			else
+			{
+				Console.WriteLine("нужно ввести  А<B\n");
+
+				goto label;
+			}
+			Console.ReadKey();
+		}
+	}
+}
