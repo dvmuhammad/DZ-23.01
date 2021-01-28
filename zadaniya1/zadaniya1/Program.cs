9e062e9216c4f5a50a782ef22615b4db9f4e2c88
Muhammad <davlatovmuhammad1264@mail.com>
2021-01-28 22:56:55 +0500

    Add zadaniya1/

---
+++ zadaniya1/zadaniya1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace zadaniya1
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Введите первое число");
+			int a = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Введите второе число");
+			int b = Convert.ToInt32(Console.ReadLine());
+			int summ = 0;
+			for (int i = a + 1; i < b; i++)
+			{
+				summ += i;
+			}
+			Console.WriteLine($"Сумма:{summ}");
+
+			Console.ReadKey();
+		}
+	}
+}
